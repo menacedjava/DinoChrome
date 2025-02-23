b3052c1cfe6250a224376346265c447a1ddb5965
Madumarov Davron <menaceteacher@gmail.com>
2025-02-23 18:59:42 +0500

    add

--- src/main/java/org/example/DinoGame.java
+++ src/main/java/org/example/DinoGame.java
@@ -1,9 +1,35 @@
 package org.example;
 
 
-public class DinoGame {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class DinoGame extends JPanel implements ActionListener, KeyListener {
     public static void main(String[] args) {
 
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
 
     }
 }
